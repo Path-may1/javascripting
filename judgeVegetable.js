e337d719b45d3258e9374d06efc6baefb0d11ecd
Path-may1 <80159654+Path-may1@users.noreply.github.com>
2021-04-03 16:15:24 +0000

    judge-vegetable-challenge

---
+++ judgeVegetable.js
@@ -0,0 +1,24 @@
+const judgeVegetable = (vegetables, metric) => {
+  return vegetables.reduce((acc, cv) => cv[metric] > acc[metric] ? cv : acc).submitter;
+}
+const vegetables = [
+  {
+    submitter: 'Old Man Franklin',
+    redness: 10,
+    plumpness: 5
+  },
+  {
+    submitter: 'Sally Tomato-Grower',
+    redness: 2,
+    plumpness: 8
+  },
+  {
+    submitter: 'Hamid Hamidson',
+    redness: 4,
+    plumpness: 3
+  }
+]
+
+const metric = 'redness'
+
+judgeVegetable(vegetables, metric)
